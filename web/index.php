fe2985a2bdab039c0d1ac177ad59d14f4015090b
root <root@unexpected-semi.home>
2015-04-30 22:14:01 +0200

    Sprawdzanie botow v1

--- web/index.php
+++ web/index.php
@@ -21,26 +21,45 @@ use Symfony\Component\HttpFoundation\Response;
 
 	$query = $con->query("SELECT ip".$quWhere);
 	
-	$ips = array(); $cnt = array(); $bots = array();
+	$ips = array(); $cnt = array(); $bots = array(); // lamerskie tabliczki
 	
 	while( $line = $query->fetch_assoc() ) {
-		if(!in_array($line['ip'], $ips)) array_push($ips, $line['ip']);
+		if(!in_array($line['ip'], $ips)) array_push($ips, $line['ip']); // unikatowe
 	} 
 	
-	$query->close();	
+	$query->close();
+
+	// Liczba wpisow
 	foreach($ips as $locip) {
 		$query = $con->query("SELECT count(1) AS ile".$quWhere." AND ip='$locip'");
 		$fetched = $query->fetch_assoc();
 		array_push($cnt, $fetched['ile']);
 		$query->close();
 	}
-	
+
+	// Czy bot?
+	foreach($ips as $locip) {
+		$query = $con->query("SELECT LENGTH(wpis) AS sizew, wpis".$quWhere." AND ip='$locip'");
+		$bt_loc=false;
+		while( $fetched = $query->fetch_assoc() ) {
+			if( $fetched['sizew']>120 //|| 
+				//preg_match("[7-9][0-9]*/=/[7-9][0-9]*", $fetched['wpis']) 
+			) $bt_loc=true;
+		}
+		array_push($bots, $bt_loc);
+		$query->close();
+	}
+
+
+	// Ustawianie obiektow
 	for($i=0; $i<count($ips); $i++) { 
 		$all[$i] = new TList;
 		$all[$i]->setIp($ips[$i]);
 		$all[$i]->setLicznik($cnt[$i]);
+		$all[$i]->setBot($bots[$i]);
 	}
 
+	// Sortowanie obiektow
 	for($i=0; $i < count($all); $i++) {
 		for($j=0; $j < count($all)-1; $j++) 
 			if( $all[$j]->getLicznik() < $all[$j+1]->getLicznik() ) {
@@ -64,12 +83,13 @@ use Symfony\Component\HttpFoundation\Response;
 	case 1: $query = $con->query("SELECT wpis, kiedy FROM inj WHERE wpis2='' AND ip='$ip'"); break;
 	case 2: $query = $con->query("SELECT wpis, wpis2, kiedy FROM inj WHERE wpis2!='' AND ip='$ip'"); break;
 	case 3: $query = $con->query("SELECT wpis, kiedy FROM inj3 WHERE ip='$ip'"); break;
+	default: die("Nie bylo tego :("); break;
 	}
 	
 
 	$i=0;
 	while( $line = $query->fetch_assoc() ) {
-		$dane[$i++] = new Tip($line['wpis'], $line['kiedy']);
+		$dane[$i++] = new Tip(addslashes(htmlentities($line['wpis'])), $line['kiedy']);
 	}
 
 	$query->close();

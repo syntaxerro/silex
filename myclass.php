ca250a16978555833a405055a1810a5d1bf77398
root <root@unexpected-semi.home>
2015-04-30 22:13:42 +0200

    TList bot

--- myclass.php
+++ myclass.php
@@ -12,19 +12,18 @@ class Tip {
 
 class TList {
 
-	private $ip; private $licznik;
+	private $ip; private $licznik; private $bot;
 
 	public function getIp() { return $this->ip; }
 
 	public function getLicznik() { return $this->licznik; }
 
+	public function getBot() { return $this->bot; }
 
 	public function setIp($a) { $this->ip = $a; }
 
-	public function setLicznik($a) { $this->licznik = $a;  }
+	public function setLicznik($a) { $this->licznik = $a; }
 
-
+	public function setBot($a) { $this->bot = $a; }
 }
-
-
 ?>

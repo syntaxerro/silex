51cd0414d6d56f906ba48fb398946ef4ea475386
Krzysztof Pazdur <pazdurk@gmail.com>
2015-05-01 22:49:01 +0200

    Include Tip and Tlist

--- src/Model/TList.php
+++ src/Model/TList.php
@@ -9,7 +9,8 @@
 namespace Syntax\Model;
 
 
-class TList {
+class TList
+{
     private $ip;
     private $licznik;
     private $bot;
@@ -20,65 +21,41 @@ class TList {
         $this->ip = $ipp;
     }
 
-    /**
-     * @return mixed
-     */
     public function getIp()
     {
         return $this->ip;
     }
 
-    /**
-     * @param $a
-     */
-    public function setIp($a)
-    {
-        $this->ip = $a;
-    }
-
-    /**
-     * @return mixed
-     */
     public function getLicznik()
     {
         return $this->licznik;
     }
 
-    /**
-     * @param $a
-     */
-    public function setLicznik($a)
-    {
-        $this->licznik = $a;
-    }
-
-    /**
-     * @return mixed
-     */
     public function getBot()
     {
         return $this->bot;
     }
 
-    /**
-     * @param $a
-     */
-    public function setBot($a)
-    {
-        $this->bot = $a;
-    }
-
-    /**
-     * @return mixed
-     */
     public function getI()
     {
         return $this->i;
     }
 
-    /**
-     * @param $a
-     */
+    public function setIp($a)
+    {
+        $this->ip = $a;
+    }
+
+    public function setLicznik($a)
+    {
+        $this->licznik = $a;
+    }
+
+    public function setBot($a)
+    {
+        $this->bot = $a;
+    }
+
     public function setI($a)
     {
         $this->i = $a;
